af25c045eb1e0669aa92897008a26711aed556dd
David Krmpotic <david.krmpotic@gmail.com>
2023-03-10 21:49:30 +0100

    fix issues and add option to sync only

--- editBase.js
+++ editBase.js
@@ -9,44 +9,27 @@ import colors from './colors.js';
 
 const args = process.argv.slice(2);
 
-const project_root = args[0];
-const frontend_dir = args[1];
-
-const parentFolder1 = project_root.substr(project_root.lastIndexOf('/')).replace('/', '');
-const parentFolder2 = frontend_dir.substr(frontend_dir.lastIndexOf('/')).replace('/', '');
-
-let parent = parentFolder1;
-
-if (parentFolder1 != parentFolder2) {
-  parent = `${parentFolder1}/${parentFolder2}`;
-}
-
-//console.log(parentFolder);
+const projectRoot = args[0];
 
 //'dmt/settings.json'
-const settingsPath = pathJoin(project_root, 'dmt-install/settings.json');
+const settingsPath = pathJoin(projectRoot, 'dmt-install/settings.json');
 
 let settings = fs.readFileSync(settingsPath, 'utf-8');
 settings = JSON.parse(settings);
 
-const app_base = pathJoin('/', settings.app_base);
+const base = pathJoin('/', settings.app_base);
 // change this to your app base name
 // ie. the frontend sub-route in which the app should run.
 
 //const canEditRe = `paths\\:[\\ ]{[\\t\\n\\ ]*assets:[\\ ]*\\'${app_base}\\'\\,[\\t\\n\\ ]*base:[\\ ]*\\'${app_base}\\'[\\t\\n\\ ]*\\}[\\ ]*\\,`;
-const canEditRe = `paths\\:[\\ ]{[\\t\\n\\ ]*base:[\\ ]*\\'${app_base}\\'[\\t\\n\\ ]*\\}[\\ ]*\\,`;
+const canEditRe = `paths\\:[\\ ]{[\\t\\n\\ ]*base:[\\ ]*\\'${base}\\'[\\t\\n\\ ]*\\}[\\ ]*\\,`;
 
 function edit(filePath) {
   const re = /kit:[\ ]*{/;
   const toAdd = `kit: {
     paths: {
-      base: '${app_base}'
+      base: '${base}'
     },`;
-  // const toAdd = `kit: {
-  //   paths: {
-  //     assets: '${app_base}',
-  //     base: '${app_base}'
-  //   },`;
   if (fs.existsSync(filePath)) {
     let fileStr = fs.readFileSync(filePath, 'utf8');
     const canEdit = !RegExp(canEditRe).test(fileStr);
@@ -54,15 +37,14 @@ function edit(filePath) {
       fileStr = fileStr.replace(re, toAdd);
       fs.writeFileSync(filePath, fileStr);
       console.log(
-        `${colors.green('✓')} Changed app base to ${colors.green(app_base)} (file: ${colors.cyan(parent)}${colors.cyan('/')}${colors.yellow(
+        `${colors.green('✓')} Changed app base to ${colors.green(base)} (file: ${colors.cyan(projectRoot)}${colors.cyan('/')}${colors.yellow(
           basename(filePath)
         )})`
       );
     } else {
-      console.log(colors.yellow(`Correct app base was already present in ${colors.cyan(parent)}${colors.cyan('/')}${basename(filePath)}`));
+      console.log(colors.yellow(`Correct app base was already present in ${colors.cyan(projectRoot)}${colors.cyan('/')}${basename(filePath)}`));
     }
   }
 }
 
-edit(pathJoin(frontend_dir, 'svelte.config.js'));
-edit(pathJoin(frontend_dir, 'svelte.config.cjs'));
+edit(pathJoin(projectRoot, 'svelte.config.js'));
--- resetBase.js
+++ resetBase.js
@@ -1,22 +1,21 @@
 import fs from 'fs';
 //const fs = require('fs');
-import { join as pathJoin, basename } from 'path';
+import { join as pathJoin } from 'path';
 
 import colors from './colors.js';
 
 const args = process.argv.slice(2);
 
-const project_root = args[0];
-const frontend_dir = args[1];
+const projectRoot = args[0];
 
-const settingsPath = pathJoin(project_root, 'dmt-install/settings.json');
+const settingsPath = pathJoin(projectRoot, 'dmt-install/settings.json');
 let settings = fs.readFileSync(settingsPath, 'utf-8');
 settings = JSON.parse(settings);
 
-const app_base = pathJoin('/', settings.app_base);
+const base = pathJoin('/', settings.app_base);
 
 //const AddedConfig = `[\\t\\n\\ ]*paths\\:[\\ ]{[\\t\\n\\ ]*assets:[\\ ]*\\'${app_base}\\'\\,[\\t\\n\\ ]*base:[\\ ]*\\'${app_base}\\'[\\t\\n\\ ]*\\}[\\ ]*\\,[\\t\\n\\ ]*`;
-const AddedConfig = `[\\t\\n\\ ]*paths\\:[\\ ]{[\\t\\n\\ ]*base:[\\ ]*\\'${app_base}\\'[\\t\\n\\ ]*\\}[\\ ]*\\,[\\t\\n\\ ]*`;
+const AddedConfig = `[\\t\\n\\ ]*paths\\:[\\ ]{[\\t\\n\\ ]*base:[\\ ]*\\'${base}\\'[\\t\\n\\ ]*\\}[\\ ]*\\,[\\t\\n\\ ]*`;
 
 const restore = filePath => {
   if (fs.existsSync(filePath)) {
@@ -28,5 +27,4 @@ const restore = filePath => {
   }
 };
 
-restore(pathJoin(frontend_dir, 'svelte.config.js'));
-restore(pathJoin(frontend_dir, 'svelte.config.cjs'));
+restore(pathJoin(projectRoot, 'svelte.config.js'));
